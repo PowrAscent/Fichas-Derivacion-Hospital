17557f422f25a4c0b37ef20d421195316d3f29a1
PowrAscent <diegogodieddp2@gmail.com>
2025-10-19 23:58:05 -0300

    feat: crear modelo usuario

--- sistema_derivacion/usuarios/models.py
+++ sistema_derivacion/usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    correo = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=30)
+    contraseña = models.CharField(max_length=32)
+
+def __str__(self):
+    return str(self.correo)+"-"+str(self.nombre)+"-"+str(self.contraseña)
